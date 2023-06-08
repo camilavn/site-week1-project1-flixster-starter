950744ea59d21c7e1429b858686cd67160b86388
Camila Naranjo <cnaranjo@cnaranj-ltm6e77.internal.salesforce.com>
2023-06-08 14:43:03 -0700

    added all 20 cards with name/rating/star

--- script.js
+++ script.js
@@ -1,110 +1,74 @@
-let fakeMoviesAPI = {
-    "dates": {
-        "maximum": "2023-06-05",
-        "minimum": "2023-04-18"
-    },
-    "page": 1,
-    "results": [
-        {
-            "adult": false,
-            "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
-            "genre_ids": [
-                16,
-                10751,
-                12,
-                14,
-                35
-            ],
-            "id": 502356,
-            "original_language": "en",
-            "original_title": "The Super Mario Bros. Movie",
-            "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
-            "popularity": 3392.2,
-            "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-            "release_date": "2023-04-05",
-            "title": "The Super Mario Bros. Movie",
-            "video": false,
-            "vote_average": 7.8,
-            "vote_count": 4327
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
-            "genre_ids": [
-                28,
-                12,
-                16,
-                878
-            ],
-            "id": 569094,
-            "original_language": "en",
-            "original_title": "Spider-Man: Across the Spider-Verse",
-            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
-            "popularity": 2921.844,
-            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-            "release_date": "2023-05-31",
-            "title": "Spider-Man: Across the Spider-Verse",
-            "video": false,
-            "vote_average": 8.8,
-            "vote_count": 739
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-            "genre_ids": [
-                28,
-                80,
-                53
-            ],
-            "id": 385687,
-            "original_language": "en",
-            "original_title": "Fast X",
-            "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-            "popularity": 2334.66,
-            "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-            "release_date": "2023-05-17",
-            "title": "Fast X",
-            "video": false,
-            "vote_average": 7.1,
-            "vote_count": 854
-        },
-    ],
-    "total_pages": 98,
-    "total_results": 1951
-}
+const movieDatabaseAPI = "de99d62b32317eae24a04bd50f9292cd"
+const queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=de99d62b32317eae24a04bd50f9292cd";
 
-console.log(fakeMoviesAPI.results[0]) 
+let movieData = null;
 
-let firstMovie = fakeMoviesAPI.results[0]
+fetch(queryURL).then((response) => response.json()).then((movieObject) => {
+        console.log(movieObject.results)
+        
+        movieObject.results.forEach( (movie) => {
+          generateCards(movie);
+        })
+        // cardRowSetup(generateCards);
+        
+})
 
-console.log(firstMovie)
+
+
+
+// console.log(fakeMoviesAPI.results[0]) 
+
+// let firstMovie = fakeMoviesAPI.results[0]
+
+// console.log(firstMovie)
+
+// document.body.style.backgroundColor = "black"
 
 function generateCards(movieObject){
+
+    //creates movie conatiners for each card 
+    let movieContainer = document.createElement("section");
+
+    let image = document.createElement('img')
+    image.src = "https://image.tmdb.org/t/p/w342" + movieObject.poster_path
+    movieContainer.appendChild(image)
+
 
     //create the star
     let star = document.createElement('span');
     star.classList.add('star');
     let starContent= document.createTextNode('⭐️');
     star.appendChild(starContent);
+    movieContainer.appendChild(star)
 
     //create rating
-    let rating = document .createElement('span');
+    let rating = document.createElement('span');
     let ratngContent = document.createTextNode(movieObject.vote_average)
     rating.classList.add('rating');
     rating.appendChild(ratngContent);
+    movieContainer.appendChild(rating);
 
     //create average container
     let averageContainer = document.createElement('div');
     averageContainer.classList.add('average');
     averageContainer.appendChild(star);
     averageContainer.appendChild(rating);
-    document.body.appendChild(averageContainer);
+    movieContainer.appendChild(averageContainer)
 
-    let image = document.createElement('img')
-    image.src = "https://image.tmdb.org/t/p/w342" + movieObject.poster_path
-    document.body.insertBefore(image, averageContainer);
+    //create movie title
+    let movieName = document.createElement('div');
+    movieName.classList.add('movie-name');
+    movieName.innerText = movieObject.original_title
+    movieContainer.appendChild(movieName)
+    document.body.appendChild(movieContainer)
     
 
 }
 
-generateCards(firstMovie)
+// function cardRowSetup(generateCards){
+// for (movieObject.results[i] = 0; movieObject.results[i] % 5; movieObject.results[i]++) {
+//     generateCards += movieObject[i] + "<br>";
+//   }
+// }
+
+// generateCards(movieData)
